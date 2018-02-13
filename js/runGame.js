b874b690da6d573c3acd82a463a09bb06c350f2e
jeremy_immanuel <jeremyimmanuel@gmail.com>
2018-02-13 14:54:02 -0800

    added sprite to code and jump method
    added sprite to code and added jump method

--- js/runGame.js
+++ js/runGame.js
@@ -3,6 +3,7 @@ var runGame = {
         game.load.image('sky', 'assets/sky.png');
         game.load.image('ground', 'assets/platform.png');
         game.load.spritesheet('sprite', 'assets/sprite.png', 32, 48);
+        game.load.atlasJSONHash('hero', 'assets/Candidate Sprite/Main Character.png', 'assets/Candidate Sprite/Main Character.json')
     },
     create: function () {
         console.log("Game is running!");
@@ -13,9 +14,19 @@ var runGame = {
         //background
         game.add.sprite(0, 0, 'sky');
 
+        //add the hero in
+        this.hero=game.add.sprite(game.width*.2, ground.y-25, 'hero');
+
+        //enable physics for hero 
+        game.physics.enable(this.hero, Phaser.Physics.ARCADE);
+
+    
     },
     update: function () {
 
+    },
+    doJump: function(){
+        this.hero.body.velocity.y = -this.power&*12;
     },
     gameOver: function () {
         game.state.start("gameOver");
