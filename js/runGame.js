aaeac1b73ec218ca75386e47ad77e5355ce89646
jeremy_immanuel <jeremyimmanuel@gmail.com>
2018-02-20 18:20:28 -0800

    Resized hero
    resized hero from 48 px high to 100 px high

--- js/runGame.js
+++ js/runGame.js
@@ -3,7 +3,7 @@ var runGame = {
         game.load.image('sky', 'assets/sky.png');
         game.load.image('ground', 'assets/platform.png');
         game.load.spritesheet('sprite', 'assets/sprite.png', 32, 48);
-        game.load.image('hero', 'assets/Candidate Sprite/Main Character/Jurassic Jog MC clone.png')
+        game.load.image('hero', 'assets/Main Character/Jurassic Jog MC clone.png')
     },
     create: function () {
         //erase this later
@@ -20,7 +20,7 @@ var runGame = {
 
         //add the hero in
         //
-        this.hero = game.add.sprite(game.width*.2, this.ground.y - 48, 'hero');
+        this.hero = game.add.sprite(game.width*.2, this.ground.y - 100, 'hero');
 
         //enable physics for hero 
         game.physics.enable(this.hero, Phaser.Physics.ARCADE);
