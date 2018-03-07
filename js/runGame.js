8cc4b80ea53e46aa6de4db139f6163120779973a
rmango <morgan-graham@live.com>
2018-03-07 13:03:36 -0800

    attempting to smooth out rock generation

--- js/runGame.js
+++ js/runGame.js
@@ -1,8 +1,8 @@
 var runGame = {
 
-    makeArray: function(start,end){
+    makeArray: function (start, end) {
         var myArray = [];
-        for(var i = start; i < end; i++) {
+        for (var i = start; i < end; i++) {
             myArray.push(i);
         }
         return myArray;
@@ -14,11 +14,11 @@ var runGame = {
         game.load.spritesheet('sprite', 'assets/sprite.png', 32, 48);
         game.load.image('heropng', 'assets/Main Character/Jurassic Jog MC.png');
         game.load.image('rock', 'assets/Rock 1/Rock 1.png');
-        game.load.atlasJSONHash('hero', 'assets/Main Character/Jurassic Jog MC.png', 'assets/Main Character/Jurassic Jog MC.json' )
+        game.load.atlasJSONHash('hero', 'assets/Main Character/Jurassic Jog MC.png', 'assets/Main Character/Jurassic Jog MC.json');
     },
 
 
-    
+
     create: function () {
         //erase this later
         console.log("Game is running!");
@@ -33,37 +33,37 @@ var runGame = {
         this.ground = game.add.sprite(0, game.height * .9, 'ground');
 
         //add the hero in
-        this.hero = game.add.sprite(game.width*.2, this.ground.y - 100, 'hero');
+        this.hero = game.add.sprite(game.width * .2, this.ground.y - 100, 'hero');
 
         //add animation to hero
-        this.hero.animations.add('run', this.makeArray(0,3), 20, true);
+        this.hero.animations.add('run', this.makeArray(0, 3), 20, true);
         this.hero.animations.play('run');
         //enable physics for hero 
         game.physics.enable(this.hero, Phaser.Physics.ARCADE);
         game.physics.enable(this.ground, Phaser.Physics.ARCADE);
 
         this.hero.body.gravity.y = 700;
-        this.hero.body.bounce.set(0.5,0.5);
+        this.hero.body.bounce.set(0.5, 0.5);
         this.hero.body.colliderWorldBounds = true;
         this.ground.body.immovable = true;
 
         //set Listeners
         //game.input.onUp.add(this.mouseUp, this);
         this.startY = this.hero.y;
-        
+
         game.input.onDown.add(this.mouseDown, this);
 
         this.rocks = game.add.group();
         this.makeRocks();
-        this.rocks.x = game.width - this.rocks.width;
-        this.rocks.y=this.ground.y-50;
-    
+        this.rocks.x = this.rocks.width;
+        this.rocks.y = this.ground.y - 50;
+
     },
-    makeRocks: function() {
+    makeRocks: function () {
         this.rocks.removeAll();
-        
-        for(var i = 0; i < 5; i++) {
-            var rock = game.add.sprite(Math.random()*400, 0, "rock");
+
+        for (var i = 0; i < 5; i++) {
+            var rock = game.add.sprite(Math.random() * 400, 0, "rock");
             this.rocks.add(rock);
         }
         //var rock = game.add.sprite(0, 0, "rock");
@@ -73,11 +73,11 @@ var runGame = {
         this.rocks.y = this.ground.y - 50;
 
         //loop through each rock + apply physics
-        this.rocks.forEach(function(rock) {
+        this.rocks.forEach(function (rock) {
             game.physics.enable(rock, Phaser.Physics.ARCADE);
             rock.body.velocity.x = -150;
             rock.body.gravity.y = 4;
-            rock.body.bounce.set(1,1);
+            rock.body.bounce.set(1, 1);
         })
 
         /*var wallHeight=game.rnd.integerInRange(2, 6);
@@ -91,18 +91,18 @@ var runGame = {
     //mouseDown: function(){
     //      this.timer = game.time.events.loop(Phaser.Time.SECOND/1000, this);
     //},
-    mouseDown: function(){
+    mouseDown: function () {
         //if(this.hero.y != this.startY){
         //    return;
         //}
         this.doJump();
 
     },
-    doJump: function(){
+    doJump: function () {
         this.hero.body.velocity.y = -400;
     },
 
-    update: function(){
+    update: function () {
         game.debug.text('Elapsed seconds: ' + this.game.time.totalElapsedSeconds(), 32, 32);
         background.tilePosition.x -= 2.5;
         game.physics.arcade.collide(this.hero, this.ground)
@@ -110,11 +110,17 @@ var runGame = {
         game.physics.arcade.collide(this.ground, this.rocks);
         game.physics.arcade.collide(this.rocks);
 
-        var firstRock = this.rocks.getChildAt(0);
+        var lastRock = this.rocks.getChildAt(this.rocks.length-1);
         //if off screen reset
-        if(firstRock.x < -game.width) {
+        if (lastRock.x < -game.width) {
             this.makeRocks();
         }
+
+        /*var firstRock = this.rocks.getChildAt(0);
+        //if off screen reset
+        if (firstRock.x < -game.width) {
+            this.makeRocks();
+        }*/
     },
     gameOver: function () {
         game.state.start("gameOver");
